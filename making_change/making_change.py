9efdd4d37b08f6f8c1ee54838f4e324700081791
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-04-02 18:46:17 -0400

    Finished exercises

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,26 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    ways_making_change = [0] * (amount + 1)
+
+    ways_making_change[0] = 1
+
+    for coins in denominations:
+        for high_amt in range(coins, amount + 1):
+            remainder = high_amt - coins
+            ways_making_change[high_amt] += ways_making_change[remainder]
+    return ways_making_change[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,23 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    output = []
+    if n == 0:
+        return [[]]
+    elif n == 1:
+        return [['rock'], ['paper'], ['scissors']]
+    else:
+        for choice in rock_paper_scissors(n-1):
+            output = output + [choice + ['rock'], choice +
+                               ['paper'], choice + ['scissors'], ]
+    return output
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
